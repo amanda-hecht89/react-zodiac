a4ffe7c48562e66fa2e1af500c965ecf7f984339
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-06 15:31:46 -0600

    main.js function solo

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import backgroundImg from '../background.png';
 import './Main.css';
+import { ZodiacCard } from '../ZodiacCard.js';
+import { zodiac } from '../data';
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      {/* import the ZodiacCard component */}
-      {/* get the zodiac array from props */}
-      {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
-      {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
+      zodiacs.map((zodiac,i) => ZodiacCard
+      {...zodiac}
+      key={zodiac.name = i} />)
     </main>
   );
 }
+
+{/* import the ZodiacCard component */}
+{/* get the zodiac array from props */}
+{/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
+{/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
